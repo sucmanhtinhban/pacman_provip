29dd3e0767ebfdd949fe1c3482f750ffd82144a6
HoangVanManh <hoangvanmanh2309@gmail.com>
2023-12-17 19:25:48 +0700

    fill code

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 //Main function simply creates a new pacman instance*/
 
 public class main {
-public static void main(String[] args)
+  public static void main(String[] args)
   {
       Pacman c = new Pacman();
   };
